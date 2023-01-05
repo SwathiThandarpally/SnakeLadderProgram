e638f0a826281108016fafaacedbf11e667e205b
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-05 23:28:47 +0530

    Added Program

--- SnakeAndLadderProgram/RepeatTill100.cs
+++ SnakeAndLadderProgram/RepeatTill100.cs
@@ -56,6 +56,9 @@ namespace SnakeAndLadderProgram
                         }
                         break;
                 }
+                Console.WriteLine("You are the Final Position Is:" + position);
+                Console.ReadLine();
             }
         }
+    }
 }
