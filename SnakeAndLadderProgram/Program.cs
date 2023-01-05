633b72c80fce29b5e4d65611d6f3eaa14cddc588
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-05 23:10:53 +0530

    Added Game started With One Player

--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SnakeAndLadderProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder geame with players");
+            //UseCase1
+            WithOnePlayer uC_1WithOnePlayer = new WithOnePlayer();
+            uC_1WithOnePlayer.Position();
+        }
+    }
+}
---
+++ SnakeAndLadderProgram/WithOnePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    internal class WithOnePlayer
+    {
+        public void Position()
+        {
+            int position = 0;
+            Console.WriteLine("Game is started");
+        }
+    }
+}
