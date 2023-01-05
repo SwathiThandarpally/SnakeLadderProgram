1eaf8026ceadcb1107deccaec1862b26a99bef74
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-05 23:16:54 +0530

    Added Number Times Dice Rolls Program

---
+++ SnakeAndLadderProgram/DiceRolls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    internal class DiceRolls
+    {
+        int position = 0;
+        public void RollDice()
+        {
+            Random random = new Random();
+            int position = random.Next(1, 7);
+            Console.WriteLine("The number of the Dice is:" + position);
+        }
+    }
+}
--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -4,10 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Snake and Ladder geame with players");
-            //UseCase1
-            WithOnePlayer uC_1WithOnePlayer = new WithOnePlayer();
-            uC_1WithOnePlayer.Position();
+            Console.WriteLine("Displaying Snake and Ladder gamee with players");
+            //////UC_1
+            ////WithOnePlayer uC_1WithOnePlayer = new WithOnePlayer();
+            ////uC_1WithOnePlayer.Position();
+            ////Console.WriteLine("---------------------------------------");
+            //UC_2
+            DiceRolls uC_2diceRolls = new DiceRolls();
+            uC_2diceRolls.RollDice();
+            Console.WriteLine("---------------------------------------");
         }
     }
 }
