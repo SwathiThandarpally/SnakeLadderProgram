1ab08bcc14aea4dbec0c480eeb847c80af82cd9f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-05 23:21:38 +0530

    Added Checking Position Of A player In Game Program

---
+++ SnakeAndLadderProgram/CheckPosition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    internal class CheckPosition
+    {
+        int position = 0;
+        public void Moves()
+        {
+            Random random = new Random();
+            int position = random.Next(1, 7);
+            int check = random.Next(0, 3);
+
+            if (check == 0)
+            {
+                Console.WriteLine("Player is in the same position");
+            }
+            else if (check > 0)
+            {
+                position += position;
+                Console.WriteLine("Player position is : " + position);
+            }
+            else
+            {
+                position -= position;
+                Console.WriteLine("Player position is : " + position);
+            }
+
+
+        }
+    }
+}
--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -4,14 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Displaying Snake and Ladder gamee with players");
-            //////UC_1
-            ////WithOnePlayer uC_1WithOnePlayer = new WithOnePlayer();
-            ////uC_1WithOnePlayer.Position();
-            ////Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Displaying Snake and Ladder game with players");
+            //UC_1
+            WithOnePlayer uC_1WithOnePlayer = new WithOnePlayer();
+            uC_1WithOnePlayer.Position();
+            Console.WriteLine("---------------------------------------");
             //UC_2
             DiceRolls uC_2diceRolls = new DiceRolls();
             uC_2diceRolls.RollDice();
+            Console.WriteLine("---------------------------------------");
+            //UC_3
+            CheckPosition uC_3checkposition = new CheckPosition();
+            uC_3checkposition.Moves();
             Console.WriteLine("---------------------------------------");
         }
     }
